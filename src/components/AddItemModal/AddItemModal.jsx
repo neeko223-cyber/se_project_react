26c07178519ddb6208747fe7732e6118d3cce7c1
Nicholas Lawrence <neeko223@gmail.com>
2026-07-23 21:38:49 -0400

    Review updates

--- src/components/AddItemModal/AddItemModal.jsx
+++ src/components/AddItemModal/AddItemModal.jsx
@@ -1,97 +1,106 @@
+import { useEffect } from "react";
 import { useForm } from "../../hooks/useForm";
 import ModalWithForm from "../ModalWithForm/ModalWithForm";
 
+const defaultValues = {
+  name: "",
+  imageUrl: "",
+  weather: "",
+};
+
 const AddItemModal = ({ isOpen, onAddItem, onClose }) => {
-  const defaultValues = {
-    name: "",
-    imageUrl: "",
-    weather: "",
-  };
-  const { values, handleChange } = useForm(defaultValues);
+  const { values, handleChange, setValues } = useForm(defaultValues);
+
+  useEffect(() => {
+    if (isOpen) {
+      setValues(defaultValues);
+    }
+  }, [isOpen]);
+
   function handleSubmit(evt) {
     evt.preventDefault();
     onAddItem(values);
   }
-  
-  return (
-    <ModalWithForm 
-        title="New garment" 
-        name="new-card" 
-        isOpen={isOpen} 
-        onClose={onClose} 
-        onSubmit={handleSubmit}
-      >
-          <label htmlFor="name" className="modal__label">
-            Name{" "}
-            <input
-              id="name"
-              type="text"
-              className="modal__input"
-              name="name"
-              placeholder="Name"
-              value={values.name}
-              onChange={handleChange}
-            />
-          </label>
 
-          <label htmlFor="imageUrl" className="modal__label">
-            Image{" "}
-            <input
-              id="imageUrl"
-              type="url"
-              className="modal__input"
-              name="imageUrl"
-              placeholder="Image URL"
-              value={values.imageUrl}
-              onChange={handleChange}
-            />
-          </label>
-      
-        <fieldset className="modal__radio-button">
-          <legend className="modal__legend">Select the weather type:</legend>
-            <label htmlFor="hot" className="modal__label modal__label_type_radio">
-              <input
-                id="hot"
-                type="radio"
-                name="weather"
-                checked={values.weather === "hot"}
-                value="hot"
-                onChange={handleChange}
-                className="modal__radio-input"
-              />{" "}
-              Hot
-            </label>
-            <label
-              htmlFor="warm"
-              className="modal__label modal__label_type_radio"
-            >
-              <input
-                id="warm"
-                type="radio"
-                name="weather"
-                checked={values.weather === "warm"}
-                value="warm"
-                onChange={handleChange}
-                className="modal__radio-input"
-              />{" "}
-              Warm
-            </label>
-            <label
-              htmlFor="cold"
-              className="modal__label modal__label_type_radio"
-            >
-              <input
-                id="cold"
-                type="radio"
-                name="weather"
-                checked={values.weather === "cold"}
-                value="cold"
-                onChange={handleChange}
-                className="modal__radio-input"
-              />{" "}
-              Cold
-            </label>
-        </fieldset>
+  return (
+    <ModalWithForm
+      title="New garment"
+      name="new-card"
+      isOpen={isOpen}
+      onClose={onClose}
+      onSubmit={handleSubmit}
+    >
+      <label htmlFor="name" className="modal__label">
+        Name{" "}
+        <input
+          id="name"
+          type="text"
+          className="modal__input"
+          name="name"
+          placeholder="Name"
+          value={values.name}
+          onChange={handleChange}
+        />
+      </label>
+
+      <label htmlFor="imageUrl" className="modal__label">
+        Image{" "}
+        <input
+          id="imageUrl"
+          type="url"
+          className="modal__input"
+          name="imageUrl"
+          placeholder="Image URL"
+          value={values.imageUrl}
+          onChange={handleChange}
+        />
+      </label>
+
+      <fieldset className="modal__radio-button">
+        <legend className="modal__legend">Select the weather type:</legend>
+        <label htmlFor="hot" className="modal__label modal__label_type_radio">
+          <input
+            id="hot"
+            type="radio"
+            name="weather"
+            checked={values.weather === "hot"}
+            value="hot"
+            onChange={handleChange}
+            className="modal__radio-input"
+          />{" "}
+          Hot
+        </label>
+        <label
+          htmlFor="warm"
+          className="modal__label modal__label_type_radio"
+        >
+          <input
+            id="warm"
+            type="radio"
+            name="weather"
+            checked={values.weather === "warm"}
+            value="warm"
+            onChange={handleChange}
+            className="modal__radio-input"
+          />{" "}
+          Warm
+        </label>
+        <label
+          htmlFor="cold"
+          className="modal__label modal__label_type_radio"
+        >
+          <input
+            id="cold"
+            type="radio"
+            name="weather"
+            checked={values.weather === "cold"}
+            value="cold"
+            onChange={handleChange}
+            className="modal__radio-input"
+          />{" "}
+          Cold
+        </label>
+      </fieldset>
     </ModalWithForm>
   );
 };
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer className="footer">
       <p className="footer__text">Developed by Nicholas Lawrence</p>
-      <p className="footer__year">2026</p>
+      <p className="footer__year">{new Date().getFullYear()}</p>
     </footer>
   );
 }
